d890fedea3ba6dd194cf7efb71f077e15244a1e5
Aluno <Aluno@Digital.net>
2019-05-15 18:36:00 -0300

    Subindo o projeto Ja no AZURE

---
+++ SenSolutions/Site/Repositorios/leituras.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var banco = require('../BD/app-banco');
+
+router.get('/ultimas', function (req, res, next) {
+  console.log(banco.conexao);
+  banco.conectar().then(() => {
+    var limite_linhas = 3;
+    return banco.sql.query(`select top ${limite_linhas} 
+                            Id as id_nome_loko, 
+                            temperatura as temp, 
+                            umidade, 
+                            FORMAT(momento,'HH:mm:ss') as momento 
+                            from leitura order by id desc`);
+  }).then(consulta => {
+
+    console.log(consulta.recordset);
+    res.send(consulta.recordset);
+
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.sendStatus(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+module.exports = router;
